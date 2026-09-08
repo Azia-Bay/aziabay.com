c49888f0fc3023c31faf72fb1a3d104a22bf39be
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 18:24:33 -0500

    chore: increase gap

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ export default function Button({ children, className, onClick, src, alt }: Butto
   var pr = (src && alt && "pr-4") || ""
   
   return (
-    <button onClick={onClick} className={`cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row items-center gap-2 bg-white text-black text-lg font-bold uppercase ${className}`}>
+    <button onClick={onClick} className={`cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row items-center gap-3 bg-white text-black text-lg font-bold uppercase ${className}`}>
       {src && alt && <Image src={src} alt={alt} className="pixelated" width={ICON_SIZE} height={ICON_SIZE} unoptimized />}
 
       <div className="mx-auto">
