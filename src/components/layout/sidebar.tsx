71649926f52f99552dc4b74960fc93740b2cffa4
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 13:04:07 -0500

    feat: scale image down

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Sidebar() {
   return (
-    <div>
-      <Image src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." fill />
+    <div className="w-1/5">
+      <Image className="w-full h-auto" src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} />
     </div>
   );
 }
