b4782f07dbe3ec6627e146d5a237f2fe6ec68a2b
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 17:15:05 -0500

    chore: update button padding

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-const ICON_SIZE = 32
+const ICON_SIZE = 48
 
 type ButtonProps = {
   children?: React.ReactNode;
@@ -13,10 +13,10 @@ type ButtonProps = {
 };
 
 export default function Button({ children, className, onClick, src, alt }: ButtonProps) {
-  var pr = (src && alt && "pr-3") || ""
+  var pr = (src && alt && "pr-4") || ""
   
   return (
-    <button onClick={onClick} className={`cursor-pointer w-full p-2 ${pr} rounded-lg border-4 border-black border-double flex flex-row justify-center items-center gap-2 bg-white text-black text-lg font-bold uppercase ${className}`}>
+    <button onClick={onClick} className={`cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row justify-center items-center gap-2 bg-white text-black text-lg font-bold uppercase ${className}`}>
       {src && alt && <Image src={src} alt={alt} className="pixelated" width={ICON_SIZE} height={ICON_SIZE} unoptimized />}
 
       {children}
