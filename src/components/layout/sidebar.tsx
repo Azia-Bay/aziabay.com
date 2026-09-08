8a3c457967a90d55a90ac7510592f42fb9c1d4a3
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 13:09:51 -0500

    feat: implement pixel perfect rendering

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Sidebar() {
   return (
-    <div className="w-1/5">
-      <Image className="cursor-pointer w-full h-auto" src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} />
+    <div className="w-1/4">
+      <Image className="cursor-pointer w-full h-auto pixelated" src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} />
     </div>
   );
 }
