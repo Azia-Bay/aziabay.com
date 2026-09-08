5810360b89f3dceb1453e77d99238b07e26c19c5
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-07 21:23:32 -0500

    feat: add headshot img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
       <main>
+        <Image
+          src="/pixel_headshot.png"
+          alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses."
+          fill />
       </main>
     </div>
   );
