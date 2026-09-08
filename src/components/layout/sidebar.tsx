f3467bb6784642c3a1bf6b12c44f791f8600981a
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 12:52:45 -0500

    refactor: refactor code style from multi-line markdown elements to one-line

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function Sidebar() {
   return (
     <div>
-      <Image
-        src="/pixel_headshot.png"
-        alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses."
-        fill />
+      <Image src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." fill />
     </div>
   );
 }
