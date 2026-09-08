7ae213ab9e9e28257a8ae7dee1b67bd205d3d014
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-08 13:05:19 -0500

    feat: update cursor mode when hovering over image

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Sidebar() {
   return (
     <div className="w-1/5">
-      <Image className="w-full h-auto" src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} />
+      <Image className="cursor-pointer w-full h-auto" src="/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} />
     </div>
   );
 }
