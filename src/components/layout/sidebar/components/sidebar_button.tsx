ff2e831e2beb10e8de0262474d8de3c0f9d11ce6
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-09 18:37:07 -0500

    feat: update button hover vfx

--- src/components/layout/sidebar/components/sidebar_button.tsx
+++ src/components/layout/sidebar/components/sidebar_button.tsx
@@ -16,13 +16,15 @@ export default function SidebarButton({ children, onClick, src, alt }: ButtonPro
   
   return (
     <button onClick={onClick} className={`group relative overflow-hidden cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row items-center gap-3 bg-white text-black text-lg font-bold uppercase`}>
-      <div className="absolute inset-0 translate-y-full group-hover:translate-y-3/4 transition-transform duration-200 ease-out pixelated bg-size-[2px_2px] bg-[repeating-conic-gradient(black_0%_25%,white_25%_50%)]" />
+      <span className="absolute inset-0 translate-y-full group-hover:translate-y-3/4 transition-transform duration-200 ease-out pixelated bg-size-[2px_2px] bg-[repeating-conic-gradient(black_0%_25%,white_25%_50%)]" />
 
       {src && alt && <Image src={src} alt={alt} className="z-10 pixelated" width={ICON_SIZE} height={ICON_SIZE} unoptimized loading="eager" />}
 
-      <div className="z-10 mx-auto group-hover:underline underline-offset-3">
+      <span className="group relative z-10 mx-auto">
         {children}
-      </div>
+
+        <span className="absolute left-0 bottom-1 w-full h-px bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-150 ease-out" />
+      </span>
     </button>
   )
 }
