f85f9dd290ec088c59409d2d5efa1b4496941437
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-09 16:04:12 -0500

    fix: fix button padding override not being used

--- src/components/layout/sidebar/components/sidebar_button.tsx
+++ src/components/layout/sidebar/components/sidebar_button.tsx
@@ -15,7 +15,7 @@ export default function SidebarButton({ children, onClick, src, alt }: ButtonPro
   var pr = (src && alt && "pr-4") || ""
   
   return (
-    <button onClick={onClick} className="cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row items-center gap-3 bg-white text-black text-lg font-bold uppercase">
+    <button onClick={onClick} className={`cursor-pointer w-full px-2 py-1 ${pr} rounded-lg border-4 border-black border-double flex flex-row items-center gap-3 bg-white text-black text-lg font-bold uppercase`}>
       {src && alt && <Image src={src} alt={alt} className="pixelated" width={ICON_SIZE} height={ICON_SIZE} unoptimized loading="eager" />}
 
       <div className="mx-auto">
