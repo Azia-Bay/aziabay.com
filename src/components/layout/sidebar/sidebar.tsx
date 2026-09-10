52d5cccd9f8f1bc3db6d1e48a576556bc53fd669
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-09 22:26:52 -0500

    chore: update sidebar layout

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -15,10 +15,8 @@ import TextButton from "@/components/layout/sidebar/components/sidebar_text_butt
 
 export default function Sidebar() {
   return (
-    <div className="w-1/3 h-screen p-4 pb-0 flex flex-col items-center">
+    <div className="w-1/3 h-screen p-4 pb-0 flex flex-col items-center gap-4">
       <Image className="w-full h-auto pixelated" src="sidebar/banner_arms.png" alt="" width={0} height={0} unoptimized loading="eager" />
-
-      <Divider />
       
       <Image className="cursor-pointer w-2/3 h-auto pixelated" src="sidebar/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} unoptimized loading="eager" />
 
@@ -33,7 +31,7 @@ export default function Sidebar() {
       </div>
 
       <About />
-      
+
       <Divider />
       
       <div className="w-auto h-auto grid grid-cols-2 gap-1">
@@ -59,7 +57,7 @@ export default function Sidebar() {
         </div>
       </div>
 
-      <Image className="w-full h-auto mt-3 pixelated" src="sidebar/banner_pillars.png" alt="" width={0} height={0} unoptimized loading="eager" />
+      <Image className="w-full h-auto pixelated" src="sidebar/banner_pillars.png" alt="" width={0} height={0} unoptimized loading="eager" />
     </div>
   );
 }
