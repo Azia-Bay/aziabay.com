259f2cce4b2e24eccbac8e0f9c0a00ad741b5fa6
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-09 21:10:23 -0500

    refactor: add semicolons

--- src/components/layout/sidebar/components/sidebar_button.tsx
+++ src/components/layout/sidebar/components/sidebar_button.tsx
@@ -31,5 +31,5 @@ export default function SidebarButton({ children, onClick, src, alt }: ButtonPro
         </span>
       }
     </button>
-  )
+  );
 }
--- src/components/layout/sidebar/components/sidebar_copyright.tsx
+++ src/components/layout/sidebar/components/sidebar_copyright.tsx
@@ -1,5 +1,5 @@
 export default function SidebarCopyright() {
   return (
     <div>&copy; Azia Bay-Asen 2022&mdash;2026</div>
-  )
+  );
 }
