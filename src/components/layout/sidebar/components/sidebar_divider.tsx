50a163fe9048efe438d0b8bb02bb551befafe6db
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-10 13:53:50 -0500

    chore: change divider style

--- src/components/layout/sidebar/components/sidebar_divider.tsx
+++ src/components/layout/sidebar/components/sidebar_divider.tsx
@@ -1,5 +1,5 @@
 export default function SidebarDivider() {
   return (
-    <hr className="w-1/2 border-t border-foreground border-dotted" />
+    <hr className="w-1/2 border-t border-foreground border" />
   );
 }
