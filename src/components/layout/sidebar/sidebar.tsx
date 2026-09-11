15ad7e74735f2b32bc49ce090b1373d7bded5732
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-10 20:53:02 -0500

    chore: add border & margin to headshot

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -22,7 +22,7 @@ type SidebarProps = {
 export default function Sidebar({ onTabChange }: SidebarProps) {
   return (
     <div className="w-1/3 h-screen p-4 pb-0 flex flex-col items-center gap-4">
-      <Image className="cursor-pointer w-2/3 h-auto pixelated" src="sidebar/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} unoptimized loading="eager" />
+      <Image className="cursor-pointer w-2/3 mt-auto h-auto rounded-lg border-4 border-foreground border-double pixelated" src="sidebar/headshot.gif" alt="A 1-bit (black & white) pixel art headshot of Azia Bay-Asen. He wears rectangular glasses." width={0} height={0} unoptimized loading="eager" />
 
       <Divider />
 
