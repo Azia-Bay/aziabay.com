5bd84dc3a5314227adcef0c122cebe702b3c14f1
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-10 21:18:30 -0500

    feat: add pillars banner alternative text

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -58,7 +58,7 @@ export default function Sidebar({ onTabChange }: SidebarProps) {
         <TextButton href={LINK_TO_SITE_SOURCE} target="_blank">source code</TextButton>
       </div>
 
-      <Image className="w-full h-auto pixelated" src="sidebar/banner_pillars.png" alt="" width={0} height={0} unoptimized loading="eager" />
+      <Image className="w-full h-auto pixelated" src="sidebar/banner_pillars.png" alt="A 1-bit (black & white) pixel art banner of four pillars resembling ionic columns from traditional Greek architecture. The pillars are ornately detailed with juts and grooves and spiral ornaments, appear to infinitely extend below the bounds of the site, and give the impression of reinforcing the site from the earth underneath." width={0} height={0} unoptimized loading="eager" />
     </div>
   );
 }
