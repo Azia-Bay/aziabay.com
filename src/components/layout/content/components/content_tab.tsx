81e9e8bf3559aab5f98c86d8ec9a21808e6d7e6d
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-10 20:34:25 -0500

    fix: clear tab hover vfx upon clicking

--- src/components/layout/content/components/content_tab.tsx
+++ src/components/layout/content/components/content_tab.tsx
@@ -1,20 +1,29 @@
 "use client"
 
+import { useState } from "react";
+
 type ContentTabProps = {
   children?: React.ReactNode;
   onClick?: () => void;
 };
 
 export default function ContentTab({ children, onClick }: ContentTabProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function onClickHelper() {
+    setIsHovered(false);
+    onClick?.();
+  }
+
   return (
-    <button onClick={onClick} className="group relative overflow-hidden cursor-pointer px-4 py-1 rounded-lg rounded-b-none border-4 border-b-0 border-background border-double bg-foreground text-background text-lg font-bold uppercase">
-      <span className="absolute inset-0 translate-y-full group-hover:translate-y-3/4 transition-transform duration-200 ease-out pixelated bg-size-[1px_1px] bg-[repeating-conic-gradient(var(--background)_0%_25%,var(--foreground)_25%_50%)]" />
+    <button onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={onClickHelper} className="group relative overflow-hidden cursor-pointer px-4 py-1 rounded-lg rounded-b-none border-4 border-b-0 border-background border-double bg-foreground text-background text-lg font-bold uppercase">
+      <span className={`absolute inset-0 ${isHovered ? "translate-y-3/4" : "translate-y-full"} transition-transform duration-200 ease-out pixelated bg-size-[1px_1px] bg-[repeating-conic-gradient(var(--background)_0%_25%,var(--foreground)_25%_50%)]`} />
 
       {children &&
         <span className="relative z-10">
           {children}
 
-          <span className="absolute left-0 bottom-0 w-full h-px bg-background scale-x-0 group-hover:scale-x-100 transition-transform duration-150 ease-out" />
+          <span className={`absolute left-0 bottom-0 w-full h-px bg-background ${isHovered ? "scale-x-100" : "scale-x-0"} transition-transform duration-150 ease-out`} />
         </span>
       }
     </button>
