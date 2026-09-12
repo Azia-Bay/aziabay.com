5f483f06f28d27a2cbc042a1f4dc4e27b972575c
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-12 15:13:33 -0500

    chore: add socials padding

--- src/components/layout/socials/socials.tsx
+++ src/components/layout/socials/socials.tsx
@@ -8,7 +8,7 @@ import Button from "@/components/layout/sidebar/components/sidebar_button"
 
 export default function Socials() {
   return (
-    <div className="w-auto h-auto px-10 pb-10 flex flex-row justify-end gap-1">
+    <div className="w-auto h-auto px-14 pb-10 flex flex-row justify-end gap-1">
       <Button href={LINK_TO_LINKEDIN} target="_blank" src="socials/button_icon_linkedin.png" alt="A 1-bit (black & white) pixel art icon of the LinkedIn logo, with a shadow underneath adding three-dimensionality." />
 
       <Button href={LINK_TO_GITHUB} target="_blank" src="socials/button_icon_github.png" alt="A 1-bit (black & white) pixel art icon of the GitHub logo, with a shadow underneath adding three-dimensionality." />
