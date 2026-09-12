90474f8a2aa53b9db971659dd84e728c2e15f45d
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-12 16:05:38 -0500

    feat: add games panel button alternative text

--- src/components/layout/content/components/panels/games/games_panel.tsx
+++ src/components/layout/content/components/panels/games/games_panel.tsx
@@ -9,7 +9,7 @@ type GamesPanelProps = {
 export default function GamesPanel({ onTabChange }: GamesPanelProps) {
   return (
     <div className="scrollbar scroll-smooth overflow-y-auto flex-1 h-full flex flex-col gap-1">
-      <Button onClick={() => { onTabChange("games_page_bruit") }} title="Bruit" state="in-development" tagline="In a run-down and secluded high school, fight twisted monsters that range from lumbering, man-sized, severed hands to aggressive swarms of disembodied eyes." genres={[ "turn-based", "strategy", "tactics", "horror", "rpg" ]} src="content/panels/games/button_spread_bruit.gif" alt="A 1-bit (black & white) pixel art banner of two arms reaching out to one other. The arms are reminiscent of the masterwork The Creation of Adam by Michelangelo, except both arms are identical, resembling Adam's and not God's." />
+      <Button onClick={() => { onTabChange("games_page_bruit") }} title="Bruit" state="in-development" tagline="In a run-down and secluded high school, fight twisted monsters that range from lumbering, man-sized, severed hands to aggressive swarms of disembodied eyes." genres={[ "turn-based", "strategy", "tactics", "horror", "rpg" ]} src="content/panels/games/button_spread_bruit.gif" alt="An animated gif of Bruit gameplay. The game's art style consists of 1-bit (black & white) isometric pixel art. A camera pans between different characters in an anatomy classroom: two teenage boys and a man-sized severed hand, as they each take their turns in queue. One of the boys strides towards the hand and strikes it, and the hand strikes back." />
     </div>
   );
 }
