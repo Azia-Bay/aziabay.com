c61babdc7a4fd90a10ebebdaf54afe7ee997e3d6
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-13 14:43:41 -0500

    feat: add eye logo hover vfx

--- src/components/layout/sidebar/components/sidebar_header.tsx
+++ src/components/layout/sidebar/components/sidebar_header.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+
 import Image from "next/image";
 
 export default function SidebarHeader() {
+  const [isEyeIconHovered, setIsEyeIconHovered] = useState(false);
+
   return (
     <div className="w-2/3 mt-6 flex flex-col">
       <div className="px-4 flex flex-row">
         <a href="/">
           <button className="group relative overflow-hidden cursor-pointer flex flex-row">
-            <Image className="w-auto h-auto px-2 py-0.5 rounded-lg rounded-r-none rounded-b-none border-t-4 border-l-4 border-foreground border-double pixelated" src="/icon.png" alt="A 1-bit (black & white) pixel art icon of an eye, wide-open. The site logo." width={0} height={0} unoptimized loading="eager" />
+            <Image onMouseEnter={() => setIsEyeIconHovered(true)} onMouseLeave={() => setIsEyeIconHovered(false)} className="w-auto h-auto px-2 py-0.5 rounded-lg rounded-r-none rounded-b-none border-t-4 border-l-4 border-foreground border-double pixelated" src={isEyeIconHovered ? "/icon_closed.png" : "/icon.png"} alt="A 1-bit (black & white) pixel art icon of an eye, wide-open. The site logo. The eye closes when hovered over by the mouse pointer." width={0} height={0} unoptimized loading="eager" />
 
             <div className="relative px-4 py-1 rounded-lg rounded-l-none rounded-b-none border-t-4 border-r-4 border-background border-double bg-foreground">
               <span className="absolute inset-0 translate-y-full group-hover:translate-y-3/4 transition-transform duration-200 ease-out pixelated bg-size-[1px_1px] bg-[repeating-conic-gradient(var(--background)_0%_25%,var(--foreground)_25%_50%)]" />
