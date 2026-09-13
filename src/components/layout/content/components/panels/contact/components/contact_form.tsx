59db1f4866b21a39e7b30f7eeb7112735a86264f
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-13 14:22:52 -0500

    feat: add cursor text override

--- src/components/layout/content/components/panels/contact/components/contact_form.tsx
+++ src/components/layout/content/components/panels/contact/components/contact_form.tsx
@@ -4,11 +4,11 @@ export default function ContactForm() {
   return (
     <form onSubmit={(e) => { e.preventDefault(); }} className="w-100 flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-        <input name="name" type="text" placeholder="Name" required className="px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
+        <input name="name" type="text" placeholder="Name" required className="cursor-text px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
 
-        <input name="email" type="email" placeholder="Email address" required className="px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
+        <input name="email" type="email" placeholder="Email address" required className="cursor-text px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
         
-        <textarea name="message" placeholder="Message" required className="px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
+        <textarea name="message" placeholder="Message" required className="cursor-text px-2 py-1 rounded-lg border-4 border-background border-double bg-foreground text-background" />
       </div>
 
       <button type="submit" className="group relative overflow-hidden cursor-pointer mx-auto px-6 py-2 rounded-lg border-4 border-foreground border-double bg-background">
