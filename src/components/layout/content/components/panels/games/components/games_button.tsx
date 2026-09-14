1b253740e8aa8f1f392b2b0e2f4d109deb6b3711
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-14 15:22:17 -0500

    feat: hook games panel buttons to modal provider

--- src/components/layout/content/components/panels/games/components/games_button.tsx
+++ src/components/layout/content/components/panels/games/components/games_button.tsx
@@ -24,8 +24,8 @@ export default function GamesButton({ onClick, state, date, title, genres, tagli
   const day = String(date?.getDate()).padStart(2, "0");
 
   return (
-    <button onClick={onClick} className="group relative overflow-hidden cursor-pointer shrink-0 min-h-44 flex flex-row">
-      <div className="relative overflow-hidden w-125 px-4 py-2 rounded-lg rounded-r-none border-4 border-r-0 border-background border-double bg-foreground text-background flex flex-col items-start">
+    <div className="relative overflow-hidden shrink-0 min-h-44 flex flex-row">
+      <button onClick={onClick} className="cursor-pointer group relative overflow-hidden w-125 px-4 py-2 rounded-lg rounded-r-none border-4 border-r-0 border-background border-double bg-foreground text-background flex flex-col items-start">
         <span className="absolute inset-0 translate-y-full group-hover:translate-y-3/4 transition-transform duration-200 ease-out pixelated bg-size-[2px_2px] bg-[repeating-conic-gradient(var(--background)_0%_25%,var(--foreground)_25%_50%)]" />
         
         <div className="z-10 flex flex-row items-center gap-1 text-sm">
@@ -51,11 +51,13 @@ export default function GamesButton({ onClick, state, date, title, genres, tagli
             </div>
           ))}
         </div>
-      </div>
+      </button>
       
-      <div onClick={() => openModal("This is an example title", <div>Hello from the modal!</div>)} className="cursor-pointer relative flex-1 overflow-hidden rounded-lg rounded-l-none border-4 border-l-0 border-background border-double">
-        <Image className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto h-auto max-w-none pixelated" src={src} alt={alt} width={0} height={0} unoptimized loading="eager" />
-      </div>
-    </button>
+      <button onClick={() => openModal("Bruit thumbnail", <Image className="max-w-full max-h-full w-auto h-auto pixelated" src={src} alt={alt} width={0} height={0} unoptimized />)} className="cursor-pointer group relative flex-1 overflow-hidden rounded-lg rounded-l-none border-4 border-l-0 border-background border-double">
+        <span className="z-10 absolute inset-0 translate-x-full group-hover:translate-x-0 transition-transform duration-200 ease-out pixelated bg-size-[4px_4px] bg-[repeating-conic-gradient(var(--background)_0%_25%,transparent_25%_50%)]" />
+
+        <Image className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto h-auto max-w-none group-hover:scale-150 transition-transform duration-300 ease-out pixelated" src={src} alt={alt} width={0} height={0} unoptimized loading="eager" />
+      </button>
+    </div>
   );
 }
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -31,7 +31,7 @@ export default function Modal({ onClose, title, children }: ModalProps) {
           </button>
         </div>
 
-        <div className="p-4">
+        <div className="flex-1 min-h-0 p-8 flex flex-col justify-center items-center">
           {children}
         </div>
       </div>
