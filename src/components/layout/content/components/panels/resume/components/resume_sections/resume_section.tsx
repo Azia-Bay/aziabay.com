a5fc3d1c9aa00de65e54428e9e8f437b3caa274f
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-14 18:48:03 -0500

    chore: update resume section headers

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_section.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_section.tsx
@@ -6,7 +6,10 @@ type ResumeSectionProps = {
 export default function ResumeSection({ title, children }: ResumeSectionProps) {
   return (
     <div>
-      <h2 className="text-lg">{title}</h2>
+      <h2 className="text-lg bg-background text-foreground my-2 px-4 py-1 text-center">
+        {title}
+      </h2>
+      
       {children}
     </div>
   )
