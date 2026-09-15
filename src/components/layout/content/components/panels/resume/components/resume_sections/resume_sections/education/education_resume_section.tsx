be66a381e9c73bc961e28ce007b95388ec92b784
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-14 20:05:57 -0500

    feat: add resume technical skills section

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/education/education_resume_section.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/education/education_resume_section.tsx
@@ -6,6 +6,7 @@ export default function EducationResumeSection() {
     <Section title="Education">
       <div className="flex flex-col gap-4">
         <Subsection org="University of Texas at Dallas" program="B.S. in Computer Science" loc="Richardson, TX" end="December 2026" bullets={[<span><b>Scholarships</b>: National Merit Scholarship (1560/1600 SAT), Academic Excellence Scholarship</span>, <span><b>Electives</b>: Machine Learning, Artificial Intelligence, Computer Networks, Embedded Systems, Compiler Design</span>, <span><b>Organizations</b>: Collegium V, Association for Computing Machinery, Student Game Developer Association</span>, <span><b>Leadership</b>: Association for Computing Machinery Education Officer</span>, <span><b>Volunteerism</b>: Alternative Spring Break Program, Heritage Farmstead Museum (60+ volunteer hours)</span>]} />
+        
         <Subsection org="International Studies Institute" program="Study Abroad" loc="Florence, IT" end="May 2024" bullets={[]} />
       </div>
     </Section>
