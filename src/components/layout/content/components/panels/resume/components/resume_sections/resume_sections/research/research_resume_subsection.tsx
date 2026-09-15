788ec8c4d0d5767cfd3c30912262b803bc11786f
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-14 20:55:55 -0500

    feat: add resume research section

---
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/research/research_resume_subsection.tsx
@@ -0,0 +1,37 @@
+type ResearchResumeSubsectionProps = {
+  title: string;
+  link: string;
+  href: string;
+  skills: string[];
+  bullets: React.ReactNode[];
+};
+
+export default function ResearchResumeSubsection({ title, link, href, skills, bullets }: ResearchResumeSubsectionProps) {
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex flex-col">
+        <h3 className="font-bold">{title}</h3>
+
+        <a href={href} target="_blank" className="cursor-pointer underline">{link}</a>
+      </div>
+
+      {skills.length > 0 &&
+        <div className="flex flex-row gap-1">
+          {skills.map((skill, index) => (
+            <div key={index} className="px-3 py-1 rounded-sm bg-background text-foreground text-sm lowercase">
+              {skill}
+            </div>
+          ))}
+        </div>
+      }
+
+      {bullets.length > 0 &&
+        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+          {bullets.map((bullet, index) => (
+            <li key={index}>{bullet}</li>
+          ))}
+        </ul>
+      }
+    </div>
+  );
+}
--- src/components/layout/content/components/panels/resume/resume_panel.tsx
+++ src/components/layout/content/components/panels/resume/resume_panel.tsx
@@ -1,17 +1,19 @@
-import Header from "@/components/layout/content/components/panels/resume/components/resume_header"
+import Header from "./components/resume_header"
+import EducationSection from "./components/resume_sections/resume_sections/education/education_resume_section";
 import ExperienceSection from "./components/resume_sections/resume_sections/experience/experience_resume_section";
 import TechnicalSkillsSection from "./components/resume_sections/resume_sections/technical_skills/technical_skills_resume_section";
 import ProjectsSection from "./components/resume_sections/resume_sections/projects/projects_resume_section";
-import EducationSection from "./components/resume_sections/resume_sections/education/education_resume_section";
+import ResearchSection from "./components/resume_sections/resume_sections/research/research_resume_section"
 
 export default function ResumePanel() {
   return (
     <div className="scrollbar scroll-smooth overflow-y-auto w-fit h-full min-h-0 rounded-lg mx-auto px-10 py-16 border-4 border-background border-double flex flex-col gap-4 bg-foreground text-background">
       <Header />
       <EducationSection />
-      <ExperienceSection />
       <TechnicalSkillsSection />
+      <ExperienceSection />
       <ProjectsSection />
+      <ResearchSection />
     </div>
   );
 }
