699df4eb5c7f15d448f3126bb76e33f82fe73310
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-18 00:08:46 -0500

    feat: add dstg page reviews

--- src/components/layout/content/components/panels/games/pages/dstg_page.tsx
+++ src/components/layout/content/components/panels/games/pages/dstg_page.tsx
@@ -21,6 +21,27 @@ const SCREENSHOTS = [
   new Screenshot("content/panels/games/pages/dstg/gameplay_2.png", "")
 ];
 
+class Review {
+  public quote: string;
+  public author: string;
+
+  constructor(quote: string, author: string) {
+    this.quote = quote;
+    this.author = author;
+  }
+};
+
+const REVIEWS = [
+  new Review(
+    "Reminds me of the old AdultSwim games from back in the day. Dedicated scream button is an A+ design choice.",
+    "Tyler Reed, High On Life 2 artist"
+  ),
+  new Review(
+    "It's just so cute and it made me happy I love it ;-;",
+    "boxcat, itch.io fan"
+  )
+];
+
 class Credit {
   public category: string;
   public names: string[];
@@ -58,6 +79,21 @@ export default function DSTGPage() {
             return <button key={index} onClick={() => openModal(`${TITLE} screenshot ${index + 1}`, <Image className="object-contain pixelated" src={screenshot.src} alt={screenshot.alt} fill unoptimized />)} className="cursor-pointer group w-3/5">
               <Image className="w-auto h-auto group-hover:scale-105 transition-transform duration-300 ease-out pixelated" src={screenshot.src} alt={screenshot.alt} width={0} height={0} unoptimized />
             </button>
+          })}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <a href="#reviews" className="cursor-pointer self-start">
+          <h2 id="reviews" className="text-2xl underline underline-offset-4">Reviews</h2>
+        </a>
+
+        <div className="ml-4 flex flex-col gap-4">
+          {REVIEWS.map((review, index) => {
+            return <div key={index} className="flex flex-col gap-1">
+              <div className="text-xl">{review.quote}</div>
+              <div className="ml-4 text-sm">&mdash; <i>{review.author}</i></div>
+            </div>
           })}
         </div>
       </div>
