0c9dff36585ae8b799d4b34981f3f53489aa24f9
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-17 23:19:48 -0500

    feat: add dstg page credits

--- src/components/layout/content/components/panels/games/pages/dstg_page.tsx
+++ src/components/layout/content/components/panels/games/pages/dstg_page.tsx
@@ -6,28 +6,68 @@ import Image from "next/image";
 
 const TITLE = "Don't Start the Game";
 
-const CREDITS = [];
+class Credit {
+  public category: string;
+  public names: string[];
+
+  constructor(category: string, names: string[]) {
+    this.category = category;
+    this.names = names;
+  }
+};
+
+const CREDITS = [
+  new Credit("Lead Programming", ["Azia Bay-Asen", "Kael Madar"]),
+  new Credit("Lead Art", ["Azia Bay-Asen"]),
+  new Credit("Art", ["Kael Madar", "Sterling Hudgens"]),
+  new Credit("Lead Music", ["Ethan Norrid"]),
+  new Credit("Lead Writing", ["Aurum"])
+];
 
 const SCREENSHOTS = [
-  [ "content/panels/games/pages/dstg/gameplay_1.png", "" ],
-  [ "content/panels/games/pages/dstg/gameplay_2.png", "" ]
+  ["content/panels/games/pages/dstg/gameplay_1.png", ""],
+  ["content/panels/games/pages/dstg/gameplay_2.png", ""]
 ];
 
 export default function DSTGPage() {
   const { openModal } = useModal()!;
 
   return (
-    <div className="scrollbar scroll-smooth overflow-y-auto w-fit h-full min-h-0 flex flex-col gap-12">
-      <h1 className="text-3xl font-bold">{TITLE}</h1>
+    <div className="scrollbar scroll-smooth overflow-y-auto h-full min-h-0 flex flex-col gap-12">
+      <a href="#title" className="cursor-pointer self-start">
+        <h1 id="title" className="text-3xl font-bold underline underline-offset-4">{TITLE}</h1>
+      </a>
 
       <div className="flex flex-col gap-6">
-        <h2 className="text-2xl">Screenshots</h2>
+        <a href="#screenshots" className="cursor-pointer self-start">
+          <h2 id="screenshots" className="text-2xl underline underline-offset-4">Screenshots</h2>
+        </a>
 
         <div className="ml-8 flex flex-col gap-4">
-          { SCREENSHOTS.map((screenshot, index) => {
+          {SCREENSHOTS.map((screenshot, index) => {
             return <button key={index} onClick={() => openModal(`${TITLE} screenshot ${index + 1}`, <Image className="object-contain pixelated" src={screenshot[0]} alt={screenshot[1]} fill unoptimized />)} className="cursor-pointer group w-3/5">
               <Image className="w-auto h-auto group-hover:scale-105 transition-transform duration-300 ease-out pixelated" src={screenshot[0]} alt={screenshot[1]} width={0} height={0} unoptimized />
             </button>
+          })}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <a href="#credits" className="cursor-pointer self-start">
+          <h2 id="credits" className="text-2xl underline underline-offset-4">Credits</h2>
+        </a>
+
+        <div className="flex flex-col gap-4">
+          {CREDITS.map((credit, index) => {
+            return <div key={index} className="flex flex-row items-center gap-12">
+              <h3 className="min-w-1/4 text-xl">{credit.category}</h3>
+
+              <div className="flex flex-col">
+                {credit.names.map((credit, jndex) => {
+                  return <div key={jndex}>{credit}</div>
+                })}
+              </div>
+            </div>
           })}
         </div>
       </div>
