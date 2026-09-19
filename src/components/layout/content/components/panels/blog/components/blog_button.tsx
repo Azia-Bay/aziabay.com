e9c8033621d3723a8903d1e21d4b4c92f05e363f
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-18 19:54:04 -0500

    feat: implement custom games button & blog button spread size

--- src/components/layout/content/components/panels/blog/components/blog_button.tsx
+++ src/components/layout/content/components/panels/blog/components/blog_button.tsx
@@ -13,9 +13,11 @@ type BlogButtonProps = {
   wordCount: number;
   src: string;
   alt: string;
+  imgW: number;
+  imgH: number;
 };
 
-export default function BlogButton({onClick, date, title, tagline, wordCount, src, alt}: BlogButtonProps) {
+export default function BlogButton({onClick, date, title, tagline, wordCount, src, alt, imgW, imgH}: BlogButtonProps) {
   const { openModal } = useModal()!;
 
   const year = date.getFullYear();
@@ -55,7 +57,7 @@ export default function BlogButton({onClick, date, title, tagline, wordCount, sr
       <button onClick={() => openModal(`${title} thumbnail`, <Image className="object-contain pixelated" src={src} alt={alt} fill unoptimized />)} className="cursor-pointer group relative flex-1 overflow-hidden rounded-lg rounded-l-none border-4 border-l-0 border-dark border-double">
         <span className="z-10 absolute inset-0 translate-x-full group-hover:translate-x-0 transition-transform duration-200 ease-out pixelated bg-size-[2px_2px] bg-[repeating-conic-gradient(var(--dark)_0%_25%,transparent_25%_50%)]" />
 
-        <Image className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto h-auto max-w-none group-hover:scale-150 transition-transform duration-300 ease-out pixelated" src={src} alt={alt} width={0} height={0} unoptimized />
+        <Image className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-none max-h-none group-hover:scale-150 transition-transform duration-300 ease-out pixelated" src={src} alt={alt} width={imgW} height={imgH} unoptimized />
       </button>
     </div>
   );
