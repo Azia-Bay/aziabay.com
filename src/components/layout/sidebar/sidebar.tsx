6679f87e2f9754265af565e004c443376fca4ee5
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 04:10:29 -0500

    refactor: refactor sidebar.tsx

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -1,12 +1,12 @@
-import { Tab } from "@/lib/types"
+import { Tab } from "@/lib/types";
 
-import About from "@/components/layout/sidebar/components/sidebar_about"
-import Buttons from "@/components/layout/sidebar/components/sidebar_buttons"
-import Copyright from "@/components/layout/sidebar/components/sidebar_copyright"
-import Details from "@/components/layout/sidebar/components/sidebar_details"
-import Divider from "@/components/layout/sidebar/components/sidebar_divider"
-import Footer from "@/components/layout/sidebar/components/sidebar_footer"
-import Header from "@/components/layout/sidebar/components/sidebar_header"
+import About from "./components/sidebar_about";
+import Buttons from "./components/sidebar_buttons";
+import Copyright from "./components/sidebar_copyright";
+import Details from "./components/sidebar_details";
+import Divider from "./components/sidebar_divider";
+import Footer from "./components/sidebar_footer";
+import Header from "./components/sidebar_header";
 
 type SidebarProps = {
   onTabChange: (tab: Tab) => void;
@@ -14,15 +14,19 @@ type SidebarProps = {
 
 export default function Sidebar({ onTabChange }: SidebarProps) {
   return (
-    <div id="sidebar" className="self-center lg:overflow-y-hidden w-125 lg:min-w-100 max-lg:max-w-full lg:max-w-1/3 lg:h-screen p-4 pb-0 flex flex-col items-center gap-4">
+    <div
+      id="sidebar"
+      className="self-center lg:overflow-y-hidden w-125 lg:min-w-100 max-w-full lg:max-w-1/3 lg:h-screen p-4 pb-0 flex flex-col items-center gap-4">
       <Header />
       <Divider />
       <About />
       <Divider />
-      <Buttons onTabChange={onTabChange} />
+      <Buttons
+        onTabChange={onTabChange} />
       <Divider />
       <Copyright />
-      <Details onTabChange={onTabChange} />
+      <Details
+        onTabChange={onTabChange} />
       <Footer />
     </div>
   );
