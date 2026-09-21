96935b8209eeace2ce89ce6faf83b09cb55bf67f
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 15:41:15 -0500

    refactor: refactor sidebar_divider.tsx

--- src/components/layout/sidebar/components/sidebar_divider.tsx
+++ src/components/layout/sidebar/components/sidebar_divider.tsx
@@ -1,5 +1,6 @@
 export default function SidebarDivider() {
   return (
-    <hr className="w-65 border-t border-foreground border" />
+    <hr
+      className="w-65 border border-foreground" />
   );
 }
