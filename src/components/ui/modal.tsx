f8da9551d08132939dc15583cc443a8c95705c61
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 02:15:27 -0500

    refactor: refactor modal.tsx

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -10,7 +10,9 @@ type ModalProps = {
 export default function Modal({ onClose, title, children }: ModalProps) {
   useEffect(() => {
     function handleEscape(e: KeyboardEvent) {
-      if (e.key === "Escape") onClose();
+      if (e.key === "Escape") {
+        onClose();
+      }
     }
 
     document.addEventListener("keydown", handleEscape);
@@ -19,23 +21,33 @@ export default function Modal({ onClose, title, children }: ModalProps) {
   }, [onClose]);
 
   return createPortal(
-    <div onClick={onClose} className="cursor-pointer fixed left-0 top-0 z-50 w-screen h-screen p-10">
-      <div onClick={(e) => e.stopPropagation()} className="cursor flex-1 w-full h-full rounded-lg border-4 border-foreground border-double bg-background flex flex-col">
-        <div className="border-b-4 border-foreground border-double flex flex-row justify-between">
-          <div className="px-4 py-2 text-xl font-bold">
+    <div
+      onClick={onClose}
+      className="cursor-pointer fixed inset-0 z-50 p-10">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="cursor w-full h-full rounded-lg border-4 border-foreground border-double bg-background flex flex-col">
+        <div
+          className="border-b-4 border-foreground border-double flex flex-row justify-between">
+          <div
+            className="px-4 py-2 text-xl font-bold">
             {title}
           </div>
 
-          <button onClick={onClose} className="cursor-pointer px-4 py-2 text-xl font-bold hover:scale-125 transition-transform duration-300 ease-out">
+          <button
+            onClick={onClose}
+            className="cursor-pointer px-4 py-2 text-xl font-bold hover:scale-125 transition-transform duration-300 ease-out">
             X
           </button>
         </div>
 
-        <div className="relative flex-1 min-h-0 m-8">
+        <div
+          className="relative flex-1 min-h-0 m-8">
           {children}
         </div>
       </div>
     </div>,
+
     document.body
   );
 }
