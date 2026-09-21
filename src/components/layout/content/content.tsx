3ccd98f82e500cb4792a35a412945328495e5056
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 16:35:19 -0500

    refactor: refactor content.tsx

--- src/components/layout/content/content.tsx
+++ src/components/layout/content/content.tsx
@@ -1,16 +1,16 @@
-import { Tab } from "@/lib/types"
+import { Tab } from "@/lib/types";
 
-import Tabs from "@/components/layout/content/components/content_tabs"
+import Tabs from "./components/content_tabs";
 
-import GamesPanel from "@/components/layout/content/components/panels/games/games_panel"
-import BlogPanel from "@/components/layout/content/components/panels/blog/blog_panel"
-import ResumePanel from "@/components/layout/content/components/panels/resume/resume_panel"
-import ContactPanel from "@/components/layout/content/components/panels/contact/contact_panel"
+import GamesPanel from "./components/panels/games/games_panel";
+import BlogPanel from "./components/panels/blog/blog_panel";
+import ResumePanel from "./components/panels/resume/resume_panel";
+import ContactPanel from "./components/panels/contact/contact_panel";
 
-import BruitGamesPage from "@/components/layout/content/components/panels/games/pages/bruit_page"
-import DSTGGamesPage from "@/components/layout/content/components/panels/games/pages/dstg_page"
+import BruitGamesPage from "./components/panels/games/pages/bruit_page";
+import DSTGGamesPage from "./components/panels/games/pages/dstg_page";
 
-import AziabayBlogPage from "@/components/layout/content/components/panels/blog/pages/aziabay_page"
+import AziabayBlogPage from "./components/panels/blog/pages/aziabay_page";
 
 type ContentProps = {
   tab: Tab;
@@ -19,10 +19,15 @@ type ContentProps = {
 
 export default function Content({ tab, onTabChange }: ContentProps) {
   return (
-    <div id="content" className="flex-1 max-lg:min-h-screen lg:min-h-0 max-lg:max-h-screen pl-5 max-lg:pr-5 pt-4 pb-5 flex flex-col">
-      <Tabs tab={tab} onTabChange={onTabChange} />
+    <div
+      id="content"
+      className="flex-1 min-h-screen lg:min-h-0 max-lg:max-h-screen pl-5 max-lg:pr-5 pt-4 pb-5 flex flex-col">
+      <Tabs
+        tab={tab}
+        onTabChange={onTabChange} />
 
-      <main className="flex-1 h-full min-h-0 max-w-full max-h-full p-5 rounded-lg border-4 border-foreground border-double flex">
+      <main
+        className="flex-1 h-full min-h-0 max-w-full max-h-full p-5 rounded-lg border-4 border-foreground border-double flex">
         {tab === "games" && <GamesPanel onTabChange={onTabChange} />}
         {tab === "blog" && <BlogPanel onTabChange={onTabChange} />}
         {tab === "resume" && <ResumePanel />}
@@ -34,5 +39,5 @@ export default function Content({ tab, onTabChange }: ContentProps) {
         {tab === "blog_page_aziabay" && <AziabayBlogPage />}
       </main>
     </div>
-  )
+  );
 }
