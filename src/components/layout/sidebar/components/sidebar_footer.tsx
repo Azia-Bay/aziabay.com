a3f0bf9ceb9388099d2a3b63e742314c8e87bed2
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 16:22:47 -0500

    refactor: refactor sidebar_footer.tsx

--- src/components/layout/sidebar/components/sidebar_footer.tsx
+++ src/components/layout/sidebar/components/sidebar_footer.tsx
@@ -10,9 +10,20 @@ export default function SidebarFooter() {
   const { theme } = useTheme();
 
   useEffect(() => setMounted(true), []);
-  if (!mounted) return null;
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <Image className="w-full h-auto pixelated" src={theme === "dark" ? "sidebar/banner_pillars.png" : "sidebar/banner_pillars_light.png"} alt="A 1-bit (black & white) pixel art banner of four pillars resembling ionic columns from traditional Greek architecture. The pillars are ornately detailed with juts and grooves and spiral ornaments, appear to infinitely extend below the bounds of the site, and give the impression of reinforcing the site from the earth underneath." width={0} height={0} unoptimized loading="eager" />
+    <Image
+      className="w-full h-auto pixelated"
+      src={theme === "dark" ? "sidebar/banner_pillars.png" :
+                              "sidebar/banner_pillars_light.png"}
+      alt="A 1-bit (black & white) pixel art banner of four pillars resembling ionic columns from traditional Greek architecture. The pillars are ornately detailed with juts and grooves and spiral ornaments, appear to infinitely extend below the bounds of the site, and give the impression of reinforcing the site from the earth underneath."
+      width={0}
+      height={0}
+      unoptimized
+      loading="eager" />
   );
 }
