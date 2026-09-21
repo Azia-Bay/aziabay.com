6939736d50df57eddb55c1ae95363190932c9db1
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 16:28:58 -0500

    refactor: refactor sidebar_light_switch.tsx

--- src/components/layout/sidebar/components/sidebar_light_switch.tsx
+++ src/components/layout/sidebar/components/sidebar_light_switch.tsx
@@ -6,14 +6,23 @@ import { useTheme } from "@teispace/next-themes";
 
 export default function LightSwitch() {
   const { theme, setTheme } = useTheme();
+  
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => setMounted(true), []);
-  if (!mounted) return null;
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="cursor-pointer hover:underline underline-offset-2">
-      <i>{theme === "dark" ? "Let there be light" : "Let there be dark"}</i>
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="cursor-pointer hover:underline underline-offset-2">
+      <i>
+        {theme === "dark" ? "Let there be light" :
+                            "Let there be dark"}
+      </i>
     </button>
   );
 }
