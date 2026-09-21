7eef93a628931154b0eec49db42b8ba172e0260b
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-20 23:48:21 -0500

    refactor: refactor types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,11 @@
-export type Tab = "games" | "blog" | "resume" | "contact" | "games_page_bruit" | "games_page_dstg" | "blog_page_aziabay";
+export type Tab =
+    "games"
+  | "blog"
+  | "resume"
+  | "contact"
+  | "games_page_bruit"
+  | "games_page_dstg"
+  | "blog_page_aziabay";
 
 export type GameState = "in-development" | "released"
 
