38aa887e17d715e103114de69a3ff1f3aa178212
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 19:28:55 -0500

    refactor: refactor blog_panel.tsx

--- src/components/layout/content/components/panels/blog/blog_panel.tsx
+++ src/components/layout/content/components/panels/blog/blog_panel.tsx
@@ -1,6 +1,6 @@
-import { Tab } from "@/lib/types"
+import { Tab } from "@/lib/types";
 
-import Button from "@/components/layout/content/components/panels/blog/components/blog_button"
+import Button from "./components/blog_button";
 
 type BlogPanelProps = {
   onTabChange: (tab: Tab) => void;
@@ -8,8 +8,18 @@ type BlogPanelProps = {
 
 export default function BlogPanel({ onTabChange }: BlogPanelProps) {
   return (
-    <div className="scrollbar scroll-smooth overflow-y-auto flex-1 flex flex-col gap-1">
-      <Button onClick={() => { onTabChange("blog_page_aziabay") }} date={new Date(2026, (9 - 1), 21)} title="Devlog: The Creation of aziabay.com" tagline="And other ramblings of the mad." wordCount={200} src="content/panels/blog/button_spread_aziabay.png" alt="A 1-bit (black & white) pixel art banner of two arms reaching out to one other. The arms are reminiscent of the masterwork The Creation of Adam by Michelangelo, except both arms are identical, resembling Adam's and not God's." imgW={500 * 2} imgH={125 * 2} />
+    <div
+      className="scrollbar scroll-smooth overflow-y-auto flex-1 flex flex-col gap-1">
+      <Button
+        onClick={() => { onTabChange("blog_page_aziabay") }}
+        date={new Date(2026, (9 - 1), 21)}
+        title="Devlog: The Creation of aziabay.com"
+        tagline="And other ramblings of the mad."
+        wordCount={200}
+        src="content/panels/blog/button_spread_aziabay.png"
+        alt="A 1-bit (black & white) pixel art banner of two arms reaching out to one other. The arms are reminiscent of the masterwork The Creation of Adam by Michelangelo, except both arms are identical, resembling Adam's and not God's."
+        imgW={500 * 2}
+        imgH={125 * 2} />
     </div>
   );
 }
