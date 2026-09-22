104dbf553f67cd6a0975510ae2c9e1f87cf2893d
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:11:49 -0500

    refactor: refactor education_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/education/education_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/education/education_resume_subsection.tsx
@@ -8,23 +8,42 @@ type EducationResumeSubsectionProps = {
 
 export default function EducationResumeSubsection({ org, program, loc, end, bullets }: EducationResumeSubsectionProps) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <h3 className="font-bold">{org}</h3>
-          <div>{loc}</div>
+    <div
+      className="flex flex-col gap-1">
+      <div
+        className="flex flex-col">
+        <div
+          className="flex flex-row justify-between">
+          <h3
+            className="font-bold">
+            {org}
+          </h3>
+          
+          <div>
+            {loc}
+          </div>
         </div>
 
-        <div className="flex flex-row justify-between">
-          <i>{program}</i>
-          <i>{end}</i>
+        <div
+          className="flex flex-row justify-between">
+          <i>
+            {program}
+          </i>
+
+          <i>
+            {end}
+          </i>
         </div>
       </div>
 
       {bullets.length > 0 &&
-        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+        <ul
+          className="list-disc list-inside ml-5 flex flex-col gap-0.5">
           {bullets.map((bullet, index) => (
-            <li key={index}>{bullet}</li>
+            <li
+              key={index}>
+              {bullet}
+            </li>
           ))}
         </ul>
       }
