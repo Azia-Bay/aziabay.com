d55e32b545856ef0bd250f66ee562ed02631a644
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:41:19 -0500

    refactor: refactor resume_header.tsx

--- src/components/layout/content/components/panels/resume/components/resume_header.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_header.tsx
@@ -3,7 +3,10 @@ import { SITE_AUTHOR } from "@/lib/consts";
 export default function ResumeHeader() {
   return (
     <div>
-      <h1 className="text-6xl font-bold text-center">{SITE_AUTHOR}</h1>
+      <h1
+        className="text-6xl font-bold text-center">
+        {SITE_AUTHOR}
+      </h1>
     </div>
   );
 }
