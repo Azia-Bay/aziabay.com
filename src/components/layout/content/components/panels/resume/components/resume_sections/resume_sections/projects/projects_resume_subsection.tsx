5e971101194fa175e41e38a4e8c2cfc60a8cc5e2
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:26:52 -0500

    refactor: refactor projects_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/projects/projects_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/projects/projects_resume_subsection.tsx
@@ -8,17 +8,32 @@ type ProjectsResumeSubsectionProps = {
 
 export default function ProjectsResumeSubsection({ title, link, href, skills, bullets }: ProjectsResumeSubsectionProps) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col">
-        <h3 className="font-bold">{title}</h3>
+    <div
+      className="flex flex-col gap-1">
+      <div
+        className="flex flex-col">
+        <h3
+          className="font-bold">
+          {title}
+        </h3>
 
-        {link && href && <a href={href} target="_blank" className="cursor-pointer mr-auto underline">{link}</a>}
+        {link && href &&
+          <a
+            href={href}
+            target="_blank"
+            className="cursor-pointer mr-auto underline">
+            {link}
+          </a>
+        }
       </div>
 
       {skills.length > 0 &&
-        <div className="flex flex-row gap-1">
+        <div
+          className="flex flex-row gap-1">
           {skills.map((skill, index) => (
-            <div key={index} className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
+            <div
+              key={index}
+              className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
               {skill}
             </div>
           ))}
@@ -26,9 +41,13 @@ export default function ProjectsResumeSubsection({ title, link, href, skills, bu
       }
 
       {bullets.length > 0 &&
-        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+        <ul
+          className="list-disc list-inside ml-5 flex flex-col gap-0.5">
           {bullets.map((bullet, index) => (
-            <li key={index}>{bullet}</li>
+            <li
+              key={index}>
+              {bullet}
+            </li>
           ))}
         </ul>
       }
