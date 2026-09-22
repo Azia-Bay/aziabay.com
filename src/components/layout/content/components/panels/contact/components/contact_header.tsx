51a3c40b7471b25b7a6e558c1090aa0e43cb9dd6
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:15:17 -0500

    refactor: refactor contact_header.tsx

--- src/components/layout/content/components/panels/contact/components/contact_header.tsx
+++ src/components/layout/content/components/panels/contact/components/contact_header.tsx
@@ -10,11 +10,22 @@ export default function ContactHeader() {
   const { theme } = useTheme();
 
   useEffect(() => setMounted(true), []);
-  if (!mounted) return null;
+  
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <div className="w-fit m-4">
-      <Image className="w-full h-auto max-w-125 pixelated" src={theme === "dark" ? "content/panels/contact/banner_arms.png" : "content/panels/contact/banner_arms_light.png"} alt="A 1-bit (black & white) pixel art banner of two arms reaching out to one other. The arms are reminiscent of the masterwork The Creation of Adam by Michelangelo, except both arms are identical, resembling Adam's and not God's." width={0} height={0} unoptimized />
+    <div
+      className="w-fit m-4">
+      <Image
+        src={theme === "dark" ? "content/panels/contact/banner_arms.png" :
+                                "content/panels/contact/banner_arms_light.png"}
+        alt="A 1-bit (black & white) pixel art banner of two arms reaching out to one other. The arms are reminiscent of the masterwork The Creation of Adam by Michelangelo, except both arms are identical, resembling Adam's and not God's."
+        className="w-full h-auto max-w-125 pixelated"
+        width={0}
+        height={0}
+        unoptimized />
     </div>
   );
 }
