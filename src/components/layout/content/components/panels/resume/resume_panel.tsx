49632ed022d582e60c3ac459b112ea49ce1539b1
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:35:32 -0500

    refactor: refactor resume_panel.tsx

--- src/components/layout/content/components/panels/resume/resume_panel.tsx
+++ src/components/layout/content/components/panels/resume/resume_panel.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/resume_header"
 import EducationSection from "./components/resume_sections/resume_sections/education/education_resume_section";
-import ExperienceSection from "./components/resume_sections/resume_sections/experience/experience_resume_section";
 import TechnicalSkillsSection from "./components/resume_sections/resume_sections/technical_skills/technical_skills_resume_section";
+import ExperienceSection from "./components/resume_sections/resume_sections/experience/experience_resume_section";
 import ProjectsSection from "./components/resume_sections/resume_sections/projects/projects_resume_section";
 import ResearchSection from "./components/resume_sections/resume_sections/research/research_resume_section";
 import AwardsSection from "./components/resume_sections/resume_sections/awards/awards_resume_section";
@@ -10,7 +10,8 @@ import VolunteerismSection from "./components/resume_sections/resume_sections/vo
 
 export default function ResumePanel() {
   return (
-    <div className="scrollbar scroll-smooth overflow-y-auto w-fit rounded-lg mx-auto px-10 py-16 border-4 border-dark border-double flex flex-col gap-4 bg-light text-dark zoom-25 2xs:zoom-30 xs:zoom-40 sm:zoom-50 md:zoom-65 lg:zoom-45 xl:zoom-65 2xl:zoom-80 3xl:zoom-100">
+    <div
+      className="scrollbar scroll-smooth overflow-y-auto w-fit rounded-lg mx-auto px-10 py-16 border-4 border-dark border-double flex flex-col gap-4 bg-light text-dark zoom-25 2xs:zoom-30 xs:zoom-40 sm:zoom-50 md:zoom-65 lg:zoom-45 xl:zoom-65 2xl:zoom-80 3xl:zoom-100">
       <Header />
       <EducationSection />
       <TechnicalSkillsSection />
