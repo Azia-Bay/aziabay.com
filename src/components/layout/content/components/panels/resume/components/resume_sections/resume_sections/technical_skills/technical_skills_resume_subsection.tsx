bf955c799148b3490465d3f7356d0dd84e4f0f1e
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:14:35 -0500

    refactor: refactor technical_skills_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/technical_skills/technical_skills_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/technical_skills/technical_skills_resume_subsection.tsx
@@ -5,14 +5,24 @@ type TechnicalSkillsResumeSubsectionProps = {
 
 export default function TechnicalSkillsResumeSubsection({ category, skills }: TechnicalSkillsResumeSubsectionProps) {
   return (
-    <div className="flex flex-row gap-2">
-      <div className="flex flex-row">
-        <h3 className="font-bold">{category}</h3>:
+    <div
+      className="flex flex-row gap-2">
+      <div
+        className="flex flex-row">
+        <h3
+          className="font-bold">
+          {category}
+        </h3>
+        
+        :
       </div>
 
-      <div className="flex flex-row gap-1">
+      <div
+        className="flex flex-row gap-1">
         {skills.map((skill, index) => (
-          <div key={index} className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
+          <div
+            key={index}
+            className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
             {skill}
           </div>
         ))}
