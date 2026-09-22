a6e854f623c2e40d300f2b90042d1884dff6e6c7
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:46:10 -0500

    refactor: refactor awards_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/awards/awards_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/awards/awards_resume_subsection.tsx
@@ -6,10 +6,22 @@ type AwardsResumeSubsectionProps = {
 
 export default function AwardsResumeSubsection({ title, org, date }: AwardsResumeSubsectionProps) {
   return (
-    <div className="flex flex-row">
-      <h3 className="flex-1 text-start font-bold">{title}</h3>
-      <i className="flex-1 text-center">{org}</i>
-      <div className="flex-1 text-end">{date}</div>
+    <div
+      className="flex flex-row">
+      <h3
+        className="flex-1 text-start font-bold">
+        {title}
+      </h3>
+      
+      <i
+        className="flex-1 text-center">
+        {org}
+      </i>
+      
+      <div
+        className="flex-1 text-end">
+        {date}
+      </div>
     </div>
   );
 }
