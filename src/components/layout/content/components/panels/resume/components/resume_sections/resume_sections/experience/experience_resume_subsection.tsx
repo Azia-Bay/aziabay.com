12c050f93c281838a92d59b22cdaaa7119412e45
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:34:06 -0500

    refactor: refactor experience_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/experience/experience_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/experience/experience_resume_subsection.tsx
@@ -10,23 +10,41 @@ type ExperienceResumeSubsectionProps = {
 
 export default function ExperienceResumeSubsection({ role, org, start, end, loc, skills, bullets }: ExperienceResumeSubsectionProps) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <h3 className="font-bold">{role}</h3>
-          <div>{start} &mdash; {end}</div>
+    <div
+      className="flex flex-col gap-1">
+      <div
+        className="flex flex-col">
+        <div
+          className="flex flex-row justify-between">
+          <h3
+            className="font-bold">
+            {role}
+          </h3>
+          
+          <div>
+            {start} &mdash; {end}
+          </div>
         </div>
 
-        <div className="flex flex-row justify-between">
-          <i>{org}</i>
-          <i>{loc}</i>
+        <div
+          className="flex flex-row justify-between">
+          <i>
+            {org}
+          </i>
+          
+          <i>
+            {loc}
+          </i>
         </div>
       </div>
 
       {skills.length > 0 &&
-        <div className="flex flex-row gap-1">
+        <div
+          className="flex flex-row gap-1">
           {skills.map((skill, index) => (
-            <div key={index} className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
+            <div
+              key={index}
+              className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
               {skill}
             </div>
           ))}
@@ -34,9 +52,13 @@ export default function ExperienceResumeSubsection({ role, org, start, end, loc,
       }
 
       {bullets.length > 0 &&
-        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+        <ul
+          className="list-disc list-inside ml-5 flex flex-col gap-0.5">
           {bullets.map((bullet, index) => (
-            <li key={index}>{bullet}</li>
+            <li
+              key={index}>
+              {bullet}
+            </li>
           ))}
         </ul>
       }
