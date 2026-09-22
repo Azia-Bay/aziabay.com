3098b0243d07774007eb9b80c4a88f2df55d1469
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:42:06 -0500

    refactor: refactor resume_section.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_section.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_section.tsx
@@ -6,11 +6,12 @@ type ResumeSectionProps = {
 export default function ResumeSection({ title, children }: ResumeSectionProps) {
   return (
     <div>
-      <h2 className="text-lg bg-dark text-light my-2 px-4 py-1 text-center">
+      <h2
+        className="bg-dark text-light text-lg my-2 px-4 py-1 text-center">
         {title}
       </h2>
       
       {children}
     </div>
-  )
+  );
 }
