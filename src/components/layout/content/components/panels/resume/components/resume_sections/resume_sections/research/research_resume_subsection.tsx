c9eee54d2456ef6b320de6ea9c1a96454fc45612
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:24:58 -0500

    refactor: refactor research_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/research/research_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/research/research_resume_subsection.tsx
@@ -8,17 +8,30 @@ type ResearchResumeSubsectionProps = {
 
 export default function ResearchResumeSubsection({ title, link, href, skills, bullets }: ResearchResumeSubsectionProps) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col">
-        <h3 className="font-bold">{title}</h3>
+    <div
+      className="flex flex-col gap-1">
+      <div
+        className="flex flex-col">
+        <h3
+          className="font-bold">
+          {title}
+        </h3>
 
-        <a href={href} target="_blank" className="cursor-pointer mr-auto underline">{link}</a>
+        <a
+          href={href}
+          target="_blank"
+          className="cursor-pointer mr-auto underline">
+          {link}
+        </a>
       </div>
 
       {skills.length > 0 &&
-        <div className="flex flex-row gap-1">
+        <div
+          className="flex flex-row gap-1">
           {skills.map((skill, index) => (
-            <div key={index} className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
+            <div
+              key={index}
+              className="px-3 py-1 rounded-sm bg-dark text-light text-sm lowercase">
               {skill}
             </div>
           ))}
@@ -26,9 +39,13 @@ export default function ResearchResumeSubsection({ title, link, href, skills, bu
       }
 
       {bullets.length > 0 &&
-        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+        <ul
+          className="list-disc list-inside ml-5 flex flex-col gap-0.5">
           {bullets.map((bullet, index) => (
-            <li key={index}>{bullet}</li>
+            <li
+              key={index}>
+              {bullet}
+            </li>
           ))}
         </ul>
       }
