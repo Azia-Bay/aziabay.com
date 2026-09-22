9e9d02cd5bd3d8c2d46607aec8ea3ae995516771
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 22:20:20 -0500

    refactor: refactor volunteerism_resume_subsection.tsx

--- src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/volunteerism/volunteerism_resume_subsection.tsx
+++ src/components/layout/content/components/panels/resume/components/resume_sections/resume_sections/volunteerism/volunteerism_resume_subsection.tsx
@@ -10,25 +10,46 @@ type VolunteerismResumeSubsectionProps = {
 
 export default function VolunteerismResumeSubsection({ role, org, start, end, loc, hours, bullets }: VolunteerismResumeSubsectionProps) {
   return (
-    <div className="flex flex-col gap-1">
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <h3 className="font-bold">{role}</h3>
-          <div>{start} &mdash; {end}</div>
+    <div
+      className="flex flex-col gap-1">
+      <div
+        className="flex flex-col">
+        <div
+          className="flex flex-row justify-between">
+          <h3
+            className="font-bold">
+            {role}
+          </h3>
+          
+          <div>
+            {start} &mdash; {end}
+          </div>
         </div>
 
-        <div className="flex flex-row justify-between">
-          <i>{org}</i>
-          <i>{loc}</i>
+        <div
+          className="flex flex-row justify-between">
+          <i>
+            {org}
+          </i>
+          
+          <i>
+            {loc}
+          </i>
         </div>
 
-        <i>{hours} volunteer hours</i>
+        <i>
+          {hours} volunteer hours
+        </i>
       </div>
 
       {bullets.length > 0 &&
-        <ul className="list-disc list-inside ml-5 flex flex-col gap-0.5">
+        <ul
+          className="list-disc list-inside ml-5 flex flex-col gap-0.5">
           {bullets.map((bullet, index) => (
-            <li key={index}>{bullet}</li>
+            <li
+              key={index}>
+              {bullet}
+            </li>
           ))}
         </ul>
       }
