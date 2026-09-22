aa472dd571073a6f4508928f4a1f4d43e6a392de
Azia Bay-Asen <aziabayasen@gmail.com>
2026-09-21 21:13:57 -0500

    refactor: refactor contact_socials.tsx

--- src/components/layout/content/components/panels/contact/components/contact_socials.tsx
+++ src/components/layout/content/components/panels/contact/components/contact_socials.tsx
@@ -1,12 +1,18 @@
 import { SOCIALS } from "@/lib/consts";
 
-import Button from "@/components/ui/button"
+import Button from "@/components/ui/button";
 
 export default function ContactSocials() {
   return (
-    <div className="w-auto h-auto flex flex-row justify-center gap-1">
+    <div
+      className="w-auto h-auto flex flex-row justify-center gap-1">
       {SOCIALS.map((social) => (
-        <Button key={social.href} href={social.href} target="_blank" src={social.src} alt={social.alt} />
+        <Button
+          key={social.href}
+          href={social.href}
+          target="_blank"
+          src={social.src}
+          alt={social.alt} />
       ))}
     </div>
   );
